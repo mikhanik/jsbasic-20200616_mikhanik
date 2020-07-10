96a272ca5d1c1552804d2a18090f7a64d9cc58a4
Misha Nikolaev <mihan1k@yandex.com>
2020-07-10 16:59:26 +0300

    3-module/3-task: написал функцию camelize

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -3,5 +3,15 @@
  * @returns {string}
  */
 function camelize(str) {
-  // ваш код...
+  if (str === '') return str;
+
+  let arrayStrings = str.split('-');
+  
+  let camelizedString = arrayStrings.map( (item, index) => {
+    if (index === 0) return item;
+
+    return item[0].toUpperCase() + item.slice(1);
+  }).join('');
+
+  return camelizedString;
 }
