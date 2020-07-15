cc4d73ca03a0ff03c91db2534af65f39a529cd34
Misha Nikolaev <mihan1k@yandex.com>
2020-07-15 20:27:09 +0300

    4-module/2-task: написал функцию выделения ячеек по диагонали

--- 4-module/2-task/index.js
+++ 4-module/2-task/index.js
@@ -3,4 +3,7 @@
  * @return {void}
  */
 function makeDiagonalRed(table) {
+  for (let index = 0; index < table.rows.length; index++) {
+    table.rows[index].cells[index].style.backgroundColor = 'RED';
+  }
 }
