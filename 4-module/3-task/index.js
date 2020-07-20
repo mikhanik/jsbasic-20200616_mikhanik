ed66547b0053fdf531c0b799f9733d807519db44
Misha Nikolaev <mihan1k@yandex.com>
2020-07-20 04:22:07 +0300

    4-module/3-task: написал функцию highlight

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -3,4 +3,18 @@
  * @param {Element} table
  */
 function highlight(table) {
+  for (let i = 0; i < table.tBodies[0].rows.length; i++) {
+    let row = table.tBodies[0].rows[i];
+
+    let ageCell = row.cells[1];
+    let genderCell = row.cells[2];
+    let isDataAvailabe = row.cells[3].getAttribute("data-available");
+
+    row.hidden = (isDataAvailabe === null);
+
+    row.classList.add(isDataAvailabe === "true" ? "available" : "unavailable",
+      genderCell.textContent === "f" ? "female" : "male");
+
+    if (ageCell.textContent < 18) {row.style = "text-decoration: line-through";}
+  }
 }
